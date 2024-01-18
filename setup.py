abe10efcde9776e0d3066a050c58bd0fc71a4eb3
Ryan Tri <ry4ntr1@gmail.com>
2024-01-17 20:36:16 -0600

    requirements.txt, setup.py: Requirements Installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'image-segmentation',
+    version= '0.0.1',
+    author= 'Ryan Tri',
+    author_email= 'ry4ntr1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
